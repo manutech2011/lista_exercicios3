01ebc941e9e2bd8204545da1bf384d887344b861
manutech2011 <emanoferreira2011@gmail.com>
2025-06-10 12:31:03 -0300

    Update listaexercicio3_resolvido.py

--- listaexercicio3_resolvido.py
+++ listaexercicio3_resolvido.py
@@ -98,6 +98,7 @@ fig, ax = plt.subplots()
 df.plot(kind = 'scatter', x = 'Projeto1', y = 'Projeto2', color='darkgreen', marker='*', ax=ax)
 st.pyplot(fig)
 
+with st.expender('Mostrar texto')
 """6) Crie um gráfico de linha que mostre a evolução dos valores dos projetos ao longo do tempo. (Peso: 2,0)
 
 Dicas:
